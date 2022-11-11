8e4b6f03257a85efd9f3359c4da538e948690f31
filsouza <felipe.a.vic@gmail.com>
2022-11-11 15:07:23 -0300

    commit inicial

---
+++ ByteBank-IO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ByteBank_IO
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var enderecoDoArquivo = "contas.txt";
+      var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+      var buffer = new byte[1024]; //1KB
+
+      fluxoDoArquivo.Read(buffer, 0, 1024);
+
+
+      Console.ReadLine();
+    }
+  }
+}
