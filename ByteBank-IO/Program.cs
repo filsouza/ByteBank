937d7d9927821529318bf45608b6e994dc632139
filsouza <felipe.a.vic@gmail.com>
2022-11-11 15:19:19 -0300

    leitura do arquivo contas.txt em bytes

--- ByteBank-IO/Program.cs
+++ ByteBank-IO/Program.cs
@@ -9,13 +9,30 @@ namespace ByteBank_IO
     {
       var enderecoDoArquivo = "contas.txt";
       var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
+      var numeroDeBytesLidos = -1;
       var buffer = new byte[1024]; //1KB
+
+      numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
 
       fluxoDoArquivo.Read(buffer, 0, 1024);
 
+      while (numeroDeBytesLidos != 0)
+      {
+        fluxoDoArquivo.Read(buffer, 0, 1024);
+        EscreverBuffer(buffer);
+
+      }
+
 
       Console.ReadLine();
+    }
+    static void EscreverBuffer(byte[] buffer)
+    {
+      foreach (var meuByte in buffer)
+      {
+        Console.Write(meuByte);
+        Console.Write(" ");
+      }
     }
   }
 }
